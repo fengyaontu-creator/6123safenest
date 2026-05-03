dd6d0b141225105ee1af0fa47aeffd57b3ab9ac2
ZYZY-Chen <thehobbitfamily@126.com>
2026-05-03 21:56:48 +0800

    Risk Agent Head

--- agents/risk_agent.py
+++ agents/risk_agent.py
@@ -1,1 +1,12 @@
 """Risk agent — D."""
+from google.adk import LlmAgent
+# 导入外部工具函数
+from tools.cea_api import verify_cea_agent_status 
+
+# 实例化 Risk Agent
+risk_agent = LlmAgent(
+    name="risk_analyst",
+    model="gemini-flash-latest", # 选用的其他模型
+    instruction="提示词待定",
+    tools=[verify_cea_agent_status] # ADK 会自动将这个 Python 函数转化为 Agent 可调用的工具
+)
